c32e234449f470f027e57b8d09a4a7691b7e8619
shailesh511 <shaileshtripathi511@gmail.com>
2022-11-05 00:15:21 +0530

    Lifecycle methods added

--- src/Components/ClassBasedComp.js
+++ src/Components/ClassBasedComp.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Dummy from "./Dummy";
 
 class ClassBasedComp extends React.Component{
     constructor(props)//it will have all the states initialized
@@ -6,15 +7,38 @@ class ClassBasedComp extends React.Component{
         super(props);
         this.state ={
           name:"Shailesh",
-          age: 18
+          age: 18,
+          displayComp: true
        }
     }
+    
+    async componentDidMount (){
+         console.log("Componend Mounted........")
+    }
+
+    async componentDidUpdate (){
+        console.log("Componend updated........")
+    }
+
+    async componentWillUnmount (){
+        console.log("Componend unmounted......")
+    }
         render(){
+          let comp
+            if(this.state.displayComp)
+          {
+             comp = <Dummy/>
+          }
+
               return(  
                     <div>
+
+                        {comp}
                         <h1>
                             name is {this.state.name} and age is {this.state.age} &  {this.props.activity}
                         </h1>
+                        <button onClick={()=>{this.setState({age:19})}}>Component update button</button>
+                        <button onClick={()=>{this.setState({displayComp:false})}}>Component Unmount Button</button>
                     </div>
            )
         }
---
+++ src/Components/Dummy.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Dummy extends React.Component{
+
+
+    render(){
+        return(
+            <h1>Hiiiiiii shailesh ....</h1>
+        )
+    }
+}
+
+export default Dummy;
